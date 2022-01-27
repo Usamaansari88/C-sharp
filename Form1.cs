0de35be774e04ed2b9dfd7b021b1e4adf15b57ae
Usamaansari88 <97385480+Usamaansari88@users.noreply.github.com>
2022-01-27 17:00:50 +0500

    c sharp

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            student std = new student();
+            string nam = textBox1.Text;
+            int ro = int.Parse(textBox2.Text);
+            string sec = textBox3.Text;
+            std.set(nam, ro, sec);
+
+            Form1 f1 = new Form1();
+            Form2 f2 = new Form2(std);
+            this.Hide();
+            f2.Show();
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2forms
+{
+    public class student
+    {
+        public string name;
+        public int roll;
+        public string section;
+
+        public void set(string n, int r, string s)
+        {
+            this.name = n;
+            this.roll = r;
+            this.section = s;
+        }
+
+    }
+}
